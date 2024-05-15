b4550787261f92d8178b0589b352082beb9b3336
SixingChen028 <sc10264@nyu.edu>
2024-05-15 16:46:18 -0400

    first commit

---
+++ environment.py
@@ -0,0 +1,383 @@
+import numpy as np
+import time
+import os
+
+import torch
+import gymnasium as gym
+from gymnasium import Wrapper 
+from gymnasium.spaces import Box, Discrete, MultiDiscrete
+
+from graph import *
+
+
+class DecisionTreeEnv(gym.Env):
+    """
+    A decision tree environment.
+    """
+
+    metadata = {'render_modes': ['human', 'rgb_array']}
+
+    def __init__(
+            self,
+            num_node,
+            t_max = 100,
+            shuffle_nodes = True,
+            shuffle_edges = True,
+        ):
+        """
+        Construct an environment.
+        """
+
+        self.t_max = t_max
+        self.num_node = num_node
+        self.shuffle_nodes = shuffle_nodes
+        self.shuffle_edges = shuffle_edges
+        self.reward_set = np.array([-8, -4, -2, -1, 1, 2, 4, 8])
+
+        # initialize graph
+        self.graph = Graph(self.num_node, self.reward_set)
+
+        # initialize action space
+        self.action_space = Discrete(self.num_node + self.num_node + 1)
+
+        # initialize observation space
+        observation_shape = (
+            self.num_node + # fixation node (num_node,)
+            self.num_node * 3 + # parent and childs of fixation node (3 * num_node,)
+            self.num_node + # root_node (num_node,)
+            3, # fixation reward, timer, stage
+        )
+        self.observation_space = Box(low = -np.inf, high = np.inf, shape = observation_shape,)
+
+
+    def reset(self, seed = None, option = {}):
+        """
+        Reset the environment.
+        """
+
+        # reset the trial
+        self.init_trial()
+
+        # get parent and child nodes
+        fixation_parent_node = self.graph.predecessors(self.fixation_node)
+        fixation_child_nodes = self.graph.successors(self.fixation_node)
+
+        # wrap observation
+        obs = np.hstack([
+            self.one_hot_coding(num_classes = self.num_node, labels = self.fixation_node),
+            self.one_hot_coding(num_classes = self.num_node, labels = fixation_parent_node),
+            self.one_hot_coding(num_classes = self.num_node, labels = fixation_child_nodes[0]),
+            self.one_hot_coding(num_classes = self.num_node, labels = fixation_child_nodes[1]),
+            self.one_hot_coding(num_classes = self.num_node, labels = self.graph.root_node),
+            self.graph.rewards[self.fixation_node],
+            self.timer,
+            self.stage,
+        ])
+    
+        # get info
+        info = {
+            'fixation_node': self.fixation_node,
+            'fixation_reward': self.graph.rewards[self.fixation_node],
+            'fixation_gain': self.graph.gains[self.fixation_node],
+            'stage': self.stage,
+            'mask': self.get_action_mask(),
+        }
+
+        return obs, info
+
+
+    def step(self, action):
+        """
+        Step the environment.
+        """
+
+        self.timer += 1
+        done = False
+        reward = 0.
+
+        action_type = action // self.num_node
+        action_node = action % self.num_node
+
+        # fixation stage
+        if self.stage == 0:
+            # fixation action
+            if action_type == 0:
+                # move fixation to the node
+                self.fixation_node = action_node
+
+                # update record
+                updated = self.update_record(action_node)
+
+            # decision action (debugging use)
+            elif action_type == 1:
+                # raise ValueError('Execute decision action in fixation stage.')
+                pass
+
+            # stage switch action
+            elif action_type == 2:
+                # switch to decision stage
+                self.stage = 1
+
+                # reset fixation to root node
+                self.fixation_node = self.graph.root_node
+
+        # decision stage
+        elif self.stage == 1:
+            # fixation action (debugging use)
+            if action_type == 0:
+                # raise ValueError('Execute fixation action in decision stage.')
+                pass
+
+            # decision action
+            elif action_type == 1:
+                # move fixation to the node
+                self.fixation_node = action_node
+            
+            # stage switch action (debugging use)
+            elif action_type == 2:
+                # raise ValueError('Execute stage swtich action in decision stage.')
+                pass
+        
+            # reward calculation
+            if self.fixation_node in self.graph.leaf_nodes and self.graph.gains[self.fixation_node] == np.max(self.graph.gains[self.graph.leaf_nodes]):
+                reward = 1.
+
+        # end a trial
+        if (self.stage == 1 and self.fixation_node in self.graph.leaf_nodes) or self.timer == self.t_max:
+            done = True
+
+        # get parent and child nodes
+        fixation_parent_node = self.graph.predecessors(self.fixation_node)
+        fixation_child_nodes = self.graph.successors(self.fixation_node)
+
+        # wrap observation
+        obs = np.hstack([
+            self.one_hot_coding(num_classes = self.num_node, labels = self.fixation_node),
+            self.one_hot_coding(num_classes = self.num_node, labels = fixation_parent_node),
+            self.one_hot_coding(num_classes = self.num_node, labels = fixation_child_nodes[0]),
+            self.one_hot_coding(num_classes = self.num_node, labels = fixation_child_nodes[1]),
+            self.one_hot_coding(num_classes = self.num_node, labels = self.graph.root_node),
+            self.graph.rewards[self.fixation_node],
+            self.timer,
+            self.stage,
+        ])
+    
+        # get info
+        info = {
+            'fixation_node': self.fixation_node,
+            'fixation_reward': self.graph.rewards[self.fixation_node],
+            'fixation_gain': self.graph.gains[self.fixation_node],
+            'stage': self.stage,
+            'mask': self.get_action_mask(),
+        }
+
+        return obs, reward, done, False, info
+    
+    
+    def init_trial(self):
+        """
+        Initialize a trial.
+        """
+
+        # initialize timer and stage
+        self.timer = 0
+        self.stage = 0
+
+        # initialize the tree
+        self.graph.reset(shuffle_edges = self.shuffle_edges, shuffle_nodes = self.shuffle_nodes)
+
+        # initialize fixation record
+        self.init_record()
+
+        # initialize fixation node ot root node
+        self.fixation_node = self.graph.root_node
+    
+
+    def init_record(self):
+        """
+        Initialize fixation record.
+        """
+
+        # initialize node recordings
+        self.visited_nodes = np.array([self.graph.root_node])
+        self.candidate_nodes = np.array(self.graph.child_dict[self.graph.root_node])
+        self.valid_nodes = np.union1d(self.visited_nodes, self.candidate_nodes)
+
+    
+    def update_record(self, node):
+        """
+        Update fixation record.
+        """
+
+        updated = 0
+
+        # if fixating a new candiate node
+        if node in self.candidate_nodes:
+            updated = 1
+
+            # include the new node into visited nodes
+            self.visited_nodes = np.append(self.visited_nodes, node)
+
+            # remove the new node from candidate nodes
+            self.candidate_nodes = np.delete(self.candidate_nodes, np.where(self.candidate_nodes == node)[0])
+
+            # if the new node has child nodes, add them into candidate nodes
+            if node in self.graph.child_dict.keys():
+                self.candidate_nodes = np.append(self.candidate_nodes, self.graph.child_dict[node])
+                    
+        # update valid nodes
+        self.valid_nodes = np.union1d(self.visited_nodes, self.candidate_nodes)
+
+        return updated
+
+
+    def get_action_mask(self):
+        """
+        Get action mask.
+        """
+
+        mask = torch.zeros((1, self.action_space.n), dtype = torch.bool) # no batch training (batch_size = 1)
+
+        # fixation stage
+        if self.stage == 0:
+            mask[0, self.valid_nodes] = True
+            mask[0, -1] = True
+
+        # decision stage
+        elif self.stage == 1:
+            mask[0, self.num_node + self.graph.leaf_nodes] = True
+        
+        return mask
+
+
+    def one_hot_coding(self, num_classes, labels = None):
+        """
+        One-hot code nodes.
+        """
+
+        if labels is None:
+            labels_one_hot = np.zeros((num_classes,))
+        else:
+            labels_one_hot = np.eye(num_classes)[labels]
+
+        return labels_one_hot
+
+
+
+class MetaLearningWrapper(Wrapper):
+    """
+    A meta-RL wrapper.
+    """
+
+    metadata = {'render_modes': ['human', 'rgb_array']}
+
+    def __init__(self, env):
+        """
+        Construct an wrapper.
+        """
+
+        super().__init__(env)
+
+        self.env = env
+        self.num_node = env.get_wrapper_attr('num_node')
+        self.one_hot_coding = env.get_wrapper_attr('one_hot_coding')
+
+        # initialize previous variables
+        self.init_prev_variables()
+
+        # define new observation space
+        new_observation_shape = (
+            self.env.observation_space.shape[0] + # obs
+            self.env.action_space.n + # previous action
+            1, # previous reward
+        )
+        self.observation_space = Box(low = -np.inf, high = np.inf, shape = new_observation_shape)
+
+
+    def step(self, action):
+        """
+        Step the environment.
+        """
+
+        obs, reward, done, truncated, info = self.env.step(action)
+
+        # concatenate previous variables into observation
+        obs_wrapped = self.wrap_obs(obs)
+
+        # update previous variables
+        self.prev_action = action
+        self.prev_reward = reward
+
+        return obs_wrapped, reward, done, truncated, info
+    
+
+    def reset(self, seed = None, options = {}):
+        """
+        Reset the environment.
+        """
+
+        obs, info = self.env.reset()
+
+        # initialize previous physical action and reward
+        self.init_prev_variables()
+
+        # concatenate previous physical action and reward into observation
+        obs_wrapped = self.wrap_obs(obs)
+
+        return obs_wrapped, info
+    
+
+    def init_prev_variables(self):
+        """
+        Reset previous variables.
+        """
+
+        self.prev_action = None
+        self.prev_reward = 0.
+
+
+    def wrap_obs(self, obs):
+        """
+        Wrap observation with previous variables.
+        """
+
+        obs_wrapped = np.hstack([
+            obs, # current obs
+            self.one_hot_coding(num_classes = self.env.action_space.n, labels = self.prev_action),
+            self.prev_reward,
+        ])
+        return obs_wrapped
+
+
+
+if __name__ == '__main__':
+    # testing
+    
+    env = DecisionTreeEnv(num_node = 5, t_max = 50)
+    env = MetaLearningWrapper(env)
+
+    for i in range(50):
+
+        obs, info = env.reset()
+        done = False
+
+        print('connctions:', env.graph.child_dict)
+        print('gains:', env.graph.gains)
+        print('initial obs:', obs.shape)
+        
+        while not done:
+
+            action = env.action_space.sample()
+            obs, reward, done, truncated, info = env.step(action)
+
+            print(
+                'obs:', obs.shape, '|',
+                'action:', action, '|',
+                'reward:', np.round(reward, 3), '|',
+                'stage:', env.stage, '|',
+                'fixation node:', env.fixation_node, '|',
+                'done:', done, '|',
+                'timer:', env.timer, '|'
+            )
+        print()
+
---
+++ graph.py
@@ -0,0 +1,164 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+class Graph:
+    """
+    A graph class.
+    """
+
+    def __init__(self, num_node, reward_set):
+        """
+        Initialize the graph.
+        """
+        
+        # initialize states
+        self.num_node = num_node
+        self.reward_set = reward_set
+        
+
+    def reset(self, shuffle_nodes = True, shuffle_edges = True):
+        """
+        Reset the graph.
+        """
+
+        # initialize and shuffle nodes
+        self.nodes = np.arange(self.num_node)
+        if shuffle_nodes:
+            self.nodes = np.random.permutation(self.nodes)
+        
+        # initialize child node dict
+        self.child_dict = {}
+        for node_idx in np.arange(self.num_node, step = 2)[:-1]:
+            if shuffle_edges:
+                if node_idx == 0:
+                    parent_idx = node_idx
+                elif node_idx > 0:
+                    parent_idx = np.random.choice([node_idx, node_idx - 1])
+            else:
+                parent_idx = node_idx
+
+            child_idx = [node_idx + 1, node_idx + 2]
+
+            self.child_dict[self.nodes[parent_idx]] = [self.nodes[child_idx[0]], self.nodes[child_idx[1]]]
+
+        # initialize parent node dict
+        self.parent_dict = {v: k for k, values in self.child_dict.items() for v in values}
+
+        # get root node, leaf and non-leaf nodes
+        self.root_node = self.nodes[0]
+        self.non_leaf_nodes = np.array(list(self.child_dict.keys()))
+        self.leaf_nodes = np.delete(self.nodes, np.where(np.isin(self.nodes, self.non_leaf_nodes))[0])
+        
+        # get node counts
+        self.num_leaf = len(self.leaf_nodes)
+        self.num_non_leaf = len(self.non_leaf_nodes)
+
+        # initialize rewards
+        self.rewards = np.random.choice(self.reward_set, size = self.num_node, replace = True)
+        self.rewards[self.root_node] = 0.
+
+        # get gains
+        self.gains = np.zeros((self.num_node,))
+        for node in self.nodes:
+            self.gains[node] = self.compute_gain(node)
+
+
+    def successors(self, node):
+        """
+        Find successor states of a given node.
+        """
+
+        if node in self.leaf_nodes:
+            return [None, None]
+        else:
+            return self.child_dict[node]
+
+
+    def predecessors(self, node):
+        """
+        Find predecessor states of a given node.
+        """
+
+        if node == self.root_node:
+            return None
+        else:
+            return self.parent_dict[node]
+    
+
+    def transition(self, start_node, move):
+        """
+        World model giving prediction: (s, a) -> (s')
+        """
+
+        # no change if starting from a leaf node
+        if start_node in self.leaf_nodes:
+            end_node = start_node
+            reward = 0.
+        else:
+            end_node = self.successors(start_node)[move]
+            reward = self.rewards[end_node]
+
+        return end_node, reward
+    
+
+    def compute_gain(self, node):
+        """
+        Compute cumulative reward to a node.
+        """
+
+        gain = self.rewards[node]
+
+        # iteratively compute gains
+        while node in self.parent_dict:
+            parent_node = self.parent_dict[node]
+            gain += self.rewards[parent_node]
+            node = parent_node
+        
+        return gain
+    
+
+    def get_adj_list(self):
+        """
+        Get adjacency list.
+        """
+
+        self.adj_list = [[] for _ in range(self.num_node)]
+        for node, children in self.child_dict.items():
+            self.adj_list[node] = children
+
+        return self.adj_list
+
+
+    def get_adj_matrix(self):
+        """
+        Get adjacency matrix.
+        """
+
+        self.adj_matrix = np.zeros((self.num_node, self.num_node))
+        for node, children in self.child_dict.items():
+            for child in children:
+                self.adj_matrix[node, child] = 1
+        
+        return self.adj_matrix
+
+
+if __name__ == '__main__':
+    # testing
+
+    graph = Graph(num_node = 11, reward_set = np.array([-8, -4, -2, -1, 1, 2, 4, 8]))
+    graph.reset()
+
+    print('child dict:', graph.child_dict)
+    print('parent dict:', graph.parent_dict)
+    print('rewards:', graph.rewards)
+    print('gains:', graph.gains)
+    print('a transition:', graph.transition(graph.root_node, 0))
+    print('adjacency list:', graph.get_adj_list())
+    print('adjacency matrix:', graph.get_adj_matrix())
+
+    G = nx.from_numpy_array(graph.adj_matrix)
+    plt.figure(figsize = (4, 4))
+    nx.draw_circular(G, with_labels = True)
+    plt.show()
